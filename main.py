e17cd37cd8d816d48af7a57ff8390fb966023d69
Sergei <imp.sergei@gmail.com>
2019-08-05 17:33:29 +0300

    20190805/1

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-import win32com.client
+﻿import win32com.client
 import tkinter as tk
+import csv
+#import sys
 from tkinter import ttk
-# from tkinter import simpledialog
+
+class Csv:
+    def __init__(self, filename, barcode):
+        self.csv_file = csv.reader(open(filename, "r"), delimiter=";")
+        self.barcode = barcode
+    def getData(self):
+        for row in self.csv_file:
+            # if current rows 2nd value is equal to input, print that row
+            if self.barcode == row[0]:
+                return row
+        return ''
 
 
 class getSum(object):
-    def __init__(self, parent):
+    def __init__(self, parent, promttext=''):
         self.toplevel = tk.Toplevel(parent)
+        self.toplevel.title=''
 
         self.val = tk.StringVar()
         self.val.set("0.00")
 
-        label = ttk.Label(self.toplevel, text="Введите сумму:  ",width=20)
+        label = ttk.Label(self.toplevel, text="Введите сумму:  ",width=15)
         self.entrySum = ttk.Entry(self.toplevel,textvariable=self.val, justify=tk.RIGHT)
 
         label.grid(row=0, padx=3, pady=5)
@@ -20,6 +33,11 @@ class getSum(object):
         self.entrySum.bind('<Return>', self.getSelection)
         self.entrySum.selection_range(0, tk.END)
         self.entrySum.focus_set()
+        self.toplevel.update()
+        wx = parent.winfo_x()+parent.winfo_width()//2-self.toplevel.winfo_width()//2
+        wy = parent.winfo_y()+parent.winfo_height()//2-self.toplevel.winfo_height()//2
+        self.toplevel.geometry('+{}+{}'.format(wx, wy))
+        self.toplevel.grab_set()
 
     def getSelection(self,event):
         self.val.set(self.entrySum.get())
@@ -31,14 +49,16 @@ class getSum(object):
 
 
 class getText(object):
-    def __init__(self, parent):
+    def __init__(self, parent, promttext='Ввести строку:'):
         self.toplevel = tk.Toplevel(parent)
+        self.toplevel.title('')
 
         self.val = tk.StringVar()
-        self.val.set("")
+        self.val.set(u'фыва')
 
-        label = ttk.Label(self.toplevel, text="Введите строку:  ",width=20)
-        self.entrySum = ttk.Entry(self.toplevel,textvariable=self.val, justify=tk.LEFT, width = 15)
+        label = ttk.Label(self.toplevel, text=promttext,width=15)
+        self.entrySum = tk.Entry(self.toplevel,textvariable=self.val, justify=tk.LEFT, width = 30)
+        # self.entrySum = ttk.Entry(self.toplevel, justify=tk.LEFT, width = 30)
 
         label.grid(row=0, padx=3, pady=5)
         self.entrySum.grid(row=0,column=1, padx=3, pady=5)
@@ -46,6 +66,11 @@ class getText(object):
         self.entrySum.bind('<Return>', self.getSelection)
         self.entrySum.selection_range(0, tk.END)
         self.entrySum.focus_set()
+        self.toplevel.update()
+        wx = parent.winfo_x()+parent.winfo_width()//2-self.toplevel.winfo_width()//2
+        wy = parent.winfo_y()+parent.winfo_height()//2-self.toplevel.winfo_height()//2
+        self.toplevel.geometry('+{}+{}'.format(wx, wy))
+        self.toplevel.grab_set()
 
     def getSelection(self,event):
         self.val.set(self.entrySum.get())
@@ -66,6 +91,7 @@ class Check:
         self.frDrv.ProtocolType = 0
         self.frDrv.ConnectionType = 0
 
+
         self.frDrv.Connect()
         self.frDrv.Disconnect()
 
@@ -74,10 +100,13 @@ class Check:
         self.clearCheck()
 
     def clearCheck(self):
+        self.cash = 0.00
+        self.card = 0.00
+        self.total = 0.00
         self.goodslist.clear()
         self.seller = 'не выбран'
-        self.frDrv.Summ1 = 0
-        self.frDrv.Summ2 = 0
+        self.frDrv.Summ1 = self.cash
+        self.frDrv.Summ2 = self.card
         self.frDrv.Summ3 = 0
         self.frDrv.Summ4 = 0
         self.frDrv.Summ5 = 0
@@ -115,7 +144,7 @@ class Main(tk.Frame):
         frameMain.pack(side=tk.TOP, fill=tk.BOTH)
 
         # self.add_img = tk.PhotoImage(file="add.gif")
-        btn_addGoodBarcode = tk.Button(frameMain, text='Штрихкод (F7)', bg='#d7d8e0', bd=1,
+        btn_addGoodBarcode = tk.Button(frameMain, text='Штрихкод (F7)', command=lambda: self.barcmd(0), bg='#d7d8e0', bd=1,
                                     compound=tk.TOP, width=15)
         btn_addGoodBarcode.grid(row=0, column=0)
 
@@ -127,7 +156,7 @@ class Main(tk.Frame):
                                 compound=tk.TOP, width=15)
         btn_delGood.grid(row=0, column=2)
 
-        btn_Seller = tk.Button(frameMain, text='Продавец', command=self.selectSeller, bg='#d7d8e0', bd=1,
+        btn_Seller = tk.Button(frameMain, text='Продавец', command=lambda: self.barcmd(1), bg='#d7d8e0', bd=1,
                                compound=tk.TOP, width=15)
         btn_Seller.grid(row=0, column=3)
 
@@ -137,16 +166,18 @@ class Main(tk.Frame):
 
         # frmMain = ttk.Frame(self,borderwidth=1)
         # frmMain.pack(fill=tk.X)
-        self.tree = ttk.Treeview(frameMain, columns=('No', 'barcode', 'description', 'price'),
+        self.tree = ttk.Treeview(frameMain, columns=('No', 'barcode', 'description', 'weight', 'price'),
                                  height=15, show='headings')
         self.tree.column("No", width=30, anchor=tk.CENTER)
         self.tree.column("barcode", width=150, anchor=tk.CENTER)
-        self.tree.column("description", width=365, anchor=tk.CENTER)
+        self.tree.column("description", width=315, anchor=tk.CENTER)
+        self.tree.column("weight",width=50,anchor=tk.CENTER)
         self.tree.column("price", width=100, anchor=tk.CENTER)
 
         self.tree.heading("No", text='#')
         self.tree.heading("barcode", text='Штрихкод')
         self.tree.heading("description", text='Описание')
+        self.tree.heading("weight", text='Вес')
         self.tree.heading("price", text='Цена')
 
         self.tree.grid(row=1, column=0, columnspan=5)
@@ -154,7 +185,7 @@ class Main(tk.Frame):
         self.seller_label = tk.Label(frameMain, text='Продавец:')
         self.seller = tk.Label(frameMain, fg='red', text=sell.seller, font=('Arial', 10, 'bold'))
         self.total_label = tk.Label(frameMain, text='Итого:')
-        self.total = tk.Label(frameMain, text='0,00')
+        self.total = tk.Label(frameMain, text='0.00')
 
         self.seller_label.grid(row=2)
         self.seller.grid(row=2, column=1)
@@ -171,12 +202,27 @@ class Main(tk.Frame):
         frameMain.bind('<F7>',self.getBarcode)
         frameMain.focus_set()
 
+    def barcmd(self, otp):
+        result = getText(root,'Штрихкод: ').returnValue()
+        if otp==0:
+            data = Csv('list.csv',result).getData()
+            if data != '':
+                addGood(root, data)
+            else:
+                addGood(root, (['', '', '0,0', '']))
+        else:
+            data = Csv('users.csv',result).getData()
+            if data != '':
+                sell.seller=data[1]
+            else:
+                sell.seller=getText(root,'Имя кассира: ').returnValue()
+            self.seller['text']=sell.seller
 
     def closeCheck(self):
-        CloseCheck()
+        CloseCheck(root)
 
-    def selectSeller(self):
-        Child()
+    #def selectSeller(self):
+    #    Child(root)
 
     def DeleteGood(self):
         for item in self.tree.selection():
@@ -184,8 +230,8 @@ class Main(tk.Frame):
             del sell.goodslist[elem - 1]
             self.update_treeview()
 
-    def add_good_in_check(self, barcode, desc, price):
-        sell.goodslist.append([barcode, desc, price])
+    def add_good_in_check(self, barcode, desc, weight, price):
+        sell.goodslist.append([barcode, desc, weight, price])
         # self.goods_list.append([barcode, desc, price])
         # self.tree.insert('','end',values=[len(self.goods_list),'',desc,price])
         self.update_treeview()
@@ -193,57 +239,97 @@ class Main(tk.Frame):
     def update_treeview(self):
         [self.tree.delete(i) for i in self.tree.get_children()]
         [self.tree.insert('', 'end',
-                          values=[idx + 1, sell.goodslist[idx][0], sell.goodslist[idx][1], sell.goodslist[idx][2]])
+                          values=[idx + 1, sell.goodslist[idx][0], sell.goodslist[idx][1], sell.goodslist[idx][2],sell.goodslist[idx][3]])
          for idx in range(len(sell.goodslist))]
+        self.total.configure(text=str(sell.total))
 
     def getBarcode(self, event):
-        result = getText(self).returnValue()
+        result = getText(root).returnValue()
+        data = Csv('list.csv',result).getData()
+        print(result)
+        print(data)
+        if data != '':
+            addGood(root,data)
+        else:
+            addGood(root,(['','','0,0','']))
+
 
     def open_addGood(self):
-        addGood()
+        addGood(root,(['','','0,0','']))
 
 
 class addGood(tk.Toplevel):
-    def __init__(self):
+    def __init__(self, parent, data=[]):
         super().__init__(root)
-        self.init_form()
+        self.withdraw()
+        self.init_form(parent, data)
         self.mainwindow = app
 
-    def init_form(self):
+    def init_form(self, parent, data):
+        self.vBarcode=tk.StringVar()
+        self.vDescr=tk.StringVar()
+        self.vWeight=tk.StringVar()
+        self.vPrice=tk.StringVar()
+        self.vBarcode.set(data[0])
+        self.vDescr.set(data[3])
+        self.vWeight.set(data[2])
+        self.vPrice.set('0.00')
         self.title('Добавление товара')
-        self.geometry('430x140+100+100')
+        # self.geometry('430x140+100+100')
         self.resizable(True, True)
 
-        label_Barcode = tk.Label(self, text='Штрихкод:')
-        label_Barcode.place(x=10, y=10)
+        label_Barcode = ttk.Label(self, text='Штрихкод:')
+        label_Barcode.grid(row=0,padx=5,pady=5)
 
-        self.Barcode = ttk.Entry(self, width=15)
-        self.Barcode.place(x=110, y=10)
+        self.Barcode = ttk.Entry(self, width=15, textvariable=self.vBarcode)
+        self.Barcode.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
 
-        label_GoodDescr = tk.Label(self, text='Наименование:')
-        label_GoodDescr.place(x=10, y=40)
+        label_GoodDescr = ttk.Label(self, text='Наименование:')
+        label_GoodDescr.grid(row=1, column=0, padx=5, pady=5)
 
-        self.GoodDescr = ttk.Entry(self, width=50)
-        self.GoodDescr.place(x=110, y=40)
+        self.GoodDescr = ttk.Entry(self, width=50, textvariable=self.vDescr)
+        self.GoodDescr.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
 
-        label_Price = tk.Label(self, text='Цена:')
-        label_Price.place(x=10, y=70)
+        label_GoodWeight = ttk.Label(self, text='Вес:')
+        label_GoodWeight.grid(row=2, column=0, padx=5, pady=5)
 
-        self.Price = ttk.Entry(self, width=10)
-        self.Price.place(x=110, y=70)
+        self.GoodWeight = ttk.Entry(self, width=50, textvariable=self.vWeight)
+        self.GoodWeight.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W)
+
+        label_Price = ttk.Label(self, text='Цена:')
+        label_Price.grid(row=3, column=0, padx=5, pady=5)
+
+        self.Price = ttk.Entry(self, width=10, textvariable=self.vPrice, justify = tk.RIGHT)
+        self.Price.grid(row=3, column=1, padx=5, pady=5, sticky=tk.W)
 
         self.btn_ok = ttk.Button(self, text='Добавить')
-        self.btn_ok.place(x=50, y=110)
+        self.btn_ok.grid(row=4, column=0, padx=5, pady=5, sticky=tk.E)
         self.btn_ok.bind('<Button-1>',
-                         lambda event: self.mainwindow.add_good_in_check(self.Barcode.get(), self.GoodDescr.get(),
-                                                                         self.Price.get()))
+                         lambda event: self.addgood(self.vBarcode.get(), self.vDescr.get(), self.vWeight.get(),
+                                                                         self.vPrice.get()))
 
         btn_cancel = ttk.Button(self, text='Закрыть', command=self.destroy)
-        btn_cancel.place(x=150, y=110)
+        btn_cancel.grid(row=4, column=1, padx=5, pady=5, sticky=tk.W)
+        self.Barcode.focus_set()
+        self.update()
+        wx = parent.winfo_x()+parent.winfo_width()//2-self.winfo_width()//2
+        wy = parent.winfo_y()+parent.winfo_height()//2-self.winfo_height()//2
+        self.geometry('+{}+{}'.format(wx, wy))
+        self.deiconify()
+        # self.focus_set()
+        self.grab_set()
 
+    def addgood(self,barcode,descr,weight,price):
+        sell.total+=float(price)
+        self.mainwindow.add_good_in_check(barcode,descr,weight,price)
+        self.vBarcode.set('')
+        self.vDescr.set('')
+        self.vWeight.set('0,0')
+        self.vPrice.set('0.00')
+        self.destroy()
 
 class Child(tk.Toplevel):
-    def __init__(self):
+    def __init__(self,parent):
         super().__init__(root)
         self.init_child()
 
@@ -279,17 +365,37 @@ class Child(tk.Toplevel):
 
 
 class CloseCheck(tk.Toplevel):
-    def __init__(self):
+    def __init__(self, parent):
         super().__init__(root)
         # self.tpw = app.topay
-        self.initForm()
+        self.withdraw()
+        self.initForm(parent)
+        self.cash = 0.00
+        self.card = 0.00
 
-    def get_sum(self):
-        # s = simpledialog.askfloat('Ввод суммы','Введите сумму',initialvalue=0.00)
+    def get_sum(self,ptype):
         result = getSum(self).returnValue()
-        self.cashButton.configure(text='Наличные (%s)' % result)
+        if ptype == 0:
+            self.cashButton.configure(text='Наличные (%s)' % result)
+            self.cash = float(result)
+        elif ptype == 1:
+            self.cardButton.configure(text='Карта (%s)' % result)
+            self.card = float(result)
 
-    def initForm(self):
+        self.entry_payed['state'] = 'normal'
+        self.entry_payed.delete(0,'end')
+        self.entry_payed.insert(0, str(self.cash+self.card))
+        self.entry_payed['state'] = 'readonly'
+        self.entry_change['state'] = 'normal'
+        self.entry_change.delete(0, 'end')
+        if (self.cash+self.card-sell.total)>0:
+            self.entry_change.insert(0,self.cash+self.card-sell.total)
+        else:
+            self.entry_change.insert(0, '0.00')
+        self.entry_change['state'] = 'readonly'
+
+
+    def initForm(self, parent):
         # Styles
         s = ttk.Style()
         s.configure('TButton', font=('Arial', '12', 'bold'))
@@ -297,8 +403,8 @@ class CloseCheck(tk.Toplevel):
 
 
         self.title('Закрытие чека')
-        self.geometry('+400+300')
-        self.resizable(True, True)
+        # self.geometry('+400+300')
+        self.resizable(False, False)
         # self.columnconfigure()
         # frame = ttk.Frame(self, relief=tk.RAISED, borderwidth=1)
         # frame.pack(fill=tk.BOTH, expand=True)
@@ -309,29 +415,29 @@ class CloseCheck(tk.Toplevel):
         label_total.grid(row=0, column=0, sticky='W', padx=5, pady=2)
         label_change.grid(row=0, column=1, sticky='W', padx=5, pady=2)
 
-        entry_total = ttk.Entry(self, width=15, font='Arial 16 bold', justify=tk.RIGHT)
-        entry_change = ttk.Entry(self, width=15, font='Arial 16 bold', justify=tk.RIGHT)
-        entry_total.grid(row=1, column=0, padx=5, pady=2, sticky=tk.N + tk.S + tk.E + tk.W)
-        entry_change.grid(row=1, column=1, padx=5, pady=2, sticky=tk.N + tk.S + tk.E + tk.W)
-        entry_total.insert(0, '0.00')
-        entry_change.insert(0, '0.00')
-        entry_change['state'] = 'readonly'
-        entry_total['state'] = 'readonly'
+        self.entry_total = ttk.Entry(self, width=15, font='Arial 16 bold', justify=tk.RIGHT)
+        self.entry_change = ttk.Entry(self, width=15, font='Arial 16 bold', justify=tk.RIGHT)
+        self.entry_total.grid(row=1, column=0, padx=5, pady=2, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.entry_change.grid(row=1, column=1, padx=5, pady=2, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.entry_total.insert(0, str(sell.total))
+        self.entry_change.insert(0, '0.00')
+        self.entry_change['state'] = 'readonly'
+        self.entry_total['state'] = 'readonly'
 
         label_payed = ttk.Label(self, text='Вносимая сумма:')
         label_payed.grid(row=2, column=0, sticky='W', padx=5, pady=2, columnspan=2)
-        entry_payed = ttk.Entry(self, justify=tk.RIGHT, font='Arial 16 bold')
-        entry_payed.insert(0, '0.00')
-        entry_payed['state'] = 'readonly'
-        entry_payed.grid(row=3, column=0, padx=5, pady=2, columnspan=2, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.entry_payed = ttk.Entry(self, justify=tk.RIGHT, font='Arial 16 bold')
+        self.entry_payed.insert(0, '0.00')
+        self.entry_payed['state'] = 'readonly'
+        self.entry_payed.grid(row=3, column=0, padx=5, pady=2, columnspan=2, sticky=tk.N + tk.S + tk.E + tk.W)
 
-        self.cashButton = ttk.Button(self, text='Наличные', width=25, style='TButton',command=self.get_sum)
-        cardButton = ttk.Button(self, text='Карта', width=25, style='TButton')
+        self.cashButton = ttk.Button(self, text='Наличные', width=25, style='TButton',command=lambda: self.get_sum(0))
+        self.cardButton = ttk.Button(self, text='Карта', width=25, style='TButton',command=lambda: self.get_sum(1))
         closeButton = ttk.Button(self, text="Закрыть чек", width=25, style='TButton')
         cancelButton = ttk.Button(self, text="Отменить чек", width=25, style='TButton')
 
         self.cashButton.grid(row=4, columnspan=2, padx=5, pady=2)
-        cardButton.grid(row=5, columnspan=2, padx=5, pady=2)
+        self.cardButton.grid(row=5, columnspan=2, padx=5, pady=2)
         closeButton.grid(row=6, columnspan=2, padx=5, pady=2)
         cancelButton.grid(row=7, columnspan=2, padx=5, pady=2)
 
@@ -342,6 +448,11 @@ class CloseCheck(tk.Toplevel):
         entry_payedmoney['state'] = 'readonly'
         entry_payedmoney.grid(row=10, column=1, sticky='E', padx=5, pady=2)
 
+        self.update()
+        wx = parent.winfo_x()+parent.winfo_width()//2-self.winfo_width()//2
+        wy = parent.winfo_y()+parent.winfo_height()//2-self.winfo_height()//2
+        self.geometry('+{}+{}'.format(wx, wy))
+        self.deiconify()
         self.grab_set()
         self.focus_set()
 
@@ -353,6 +464,6 @@ if __name__ == "__main__":
     app = Main(root)
     app.pack()
     root.title("Регистрация продажи в ККМ")
-    root.geometry("650x450+300+200")
-    root.resizable(True, True)
+    root.geometry('650x450+{}+{}'.format((root.winfo_screenwidth()//2-325),(root.winfo_screenheight()//2-225)))
+    root.resizable(False, False)
     root.mainloop()
